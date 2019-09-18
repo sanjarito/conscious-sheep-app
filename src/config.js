fd44cd5a1fc846d86410e1a2661d3bd56dffa65d
Santiago Casar <santiago@nowhereprod.com>
2019-09-18 18:50:51 -0400

    all

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://infinite-beyond-21412.herokuapp.com/api/',
+  API_ENDPOINT: 'https://infinite-beyond-21412.herokuapp.com/api/favors',
   TOKEN_KEY: '242ed342-0935-4e6e-92cb-9f142b22e0e2',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
     "http://localhost:3002/api"
